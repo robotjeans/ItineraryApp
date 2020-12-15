e92d851d7be9fcf7c60a339fcdb0f3026426a876
robotjeans <conspiracy_seven@msn.com>
2020-12-15 10:55:45 +0100

    Initial Commit

---
+++ Itinerary/ItineraryApp.swift
@@ -0,0 +1,17 @@
+//
+//  ItineraryApp.swift
+//  Itinerary
+//
+//  Created by Shawn Sheehan on 12/15/20.
+//
+
+import SwiftUI
+
+@main
+struct ItineraryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
